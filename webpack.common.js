cdd30b8ded15670688688b329071d5eada62b552
Leandro Gatti <leandro.gatti@mercadolibre.com>
2018-04-20 12:50:01 -0300

    improve webpack common

---
+++ webpack.common.js
@@ -0,0 +1,6 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+}
